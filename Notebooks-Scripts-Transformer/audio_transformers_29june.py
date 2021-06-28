4689a5cd37796d90f111b81cdf1a471f7a463251
Mahim1997 <38731142+Mahim1997@users.noreply.github.com>
2021-06-28 17:19:46 +0600

    Scripts shortened

--- Notebooks-Scripts-Transformer/audio_transformers_29june.py
+++ Notebooks-Scripts-Transformer/audio_transformers_29june.py
@@ -70,11 +70,20 @@ print(X.shape, Y.shape)
 
 
 ################################################################################
-X_train, X_test, Y_train, Y_test = train_test_split(X, Y, 
+## Load accordingly
+################################################################################
+X_train, X_val, Y_train, Y_val = train_test_split(X, Y, 
                                     test_size=0.2, random_state=RANDOM_STATE)
 
 print(X_train.shape, Y_train.shape)
-print(X_test.shape, Y_test.shape)
+print(X_val.shape, Y_val.shape)
+
+MODEL_SAVE_DIRECTORY = "Transformers"
+DATE = "June29"
+FILE_MODEL_PATH = "{}/{}-Transformer.h5".format(MODEL_SAVE_DIRECTORY, DATE)
+FILE_MODEL_CHECKPOINT_BEST = "{}/{}-BestCKPT.h5".format(MODEL_SAVE_DIRECTORY, DATE)
+
+print(f"FILE_MODEL_PATH = {FILE_MODEL_PATH}\nFILE_MODEL_CHECKPOINT_BEST = {FILE_MODEL_CHECKPOINT_BEST}")
 
 #@title Helper Script is kept here verbatim. Please Update from GitHub regularly !!
 
@@ -159,243 +168,9 @@ def plot_confusion_matrix(Y_true=None, Y_predicted=None):
     plt.xlabel("Predicted")
     plt.ylabel("Actual")
 
-
-
-"""#### Transformer model taken from [music_genre_classification_GitHub](https://github.com/CVxTz/music_genre_classification/blob/master/code/trsf_genre_classification.py)"""
-
-def get_angles(pos, i, d_model):
-    angle_rates = 1 / np.power(10000, (2 * (i // 2)) / np.float32(d_model))
-    return pos * angle_rates
-
-
-def positional_encoding(position, d_model):
-    angle_rads = get_angles(
-        np.arange(position)[:, np.newaxis], np.arange(d_model)[np.newaxis, :], d_model
-    )
-
-    # apply sin to even indices in the array; 2i
-    angle_rads[:, 0::2] = np.sin(angle_rads[:, 0::2])
-
-    # apply cos to odd indices in the array; 2i+1
-    angle_rads[:, 1::2] = np.cos(angle_rads[:, 1::2])
-
-    pos_encoding = angle_rads[np.newaxis, ...]
-
-    return tf.cast(pos_encoding, dtype=tf.float32)
-
-
-def scaled_dot_product_attention(q, k, v, mask):
-    """Calculate the attention weights.
-    q, k, v must have matching leading dimensions.
-    k, v must have matching penultimate dimension, i.e.: seq_len_k = seq_len_v.
-    The mask has different shapes depending on its type(padding or look ahead)
-    but it must be broadcastable for addition.
-    Args:
-      q: query shape == (..., seq_len_q, depth)
-      k: key shape == (..., seq_len_k, depth)
-      v: value shape == (..., seq_len_v, depth_v)
-      mask: Float tensor with shape broadcastable
-            to (..., seq_len_q, seq_len_k). Defaults to None.
-    Returns:
-      output, attention_weights
-    """
-
-    matmul_qk = tf.matmul(q, k, transpose_b=True)  # (..., seq_len_q, seq_len_k)
-
-    # scale matmul_qk
-    dk = tf.cast(tf.shape(k)[-1], tf.float32)
-    scaled_attention_logits = matmul_qk / tf.math.sqrt(dk)
-
-    # add the mask to the scaled tensor.
-    if mask is not None:
-        scaled_attention_logits += mask * -1e9
-
-    # softmax is normalized on the last axis (seq_len_k) so that the scores
-    # add up to 1.
-    
-    attention_weights = tf.nn.softmax(
-        scaled_attention_logits, axis=-1
-    )  # (..., seq_len_q, seq_len_k)
-
-    output = tf.matmul(attention_weights, v)  # (..., seq_len_q, depth_v)
-
-    return output # , attention_weights
-
-class MultiHeadAttention(tf.keras.layers.Layer):
-    def __init__(self, d_model, num_heads):
-        super(MultiHeadAttention, self).__init__()
-        self.num_heads = num_heads
-        self.d_model = d_model
-
-        assert d_model % self.num_heads == 0
-
-        self.depth = d_model // self.num_heads
-
-        self.wq = tf.keras.layers.Dense(d_model)
-        self.wk = tf.keras.layers.Dense(d_model)
-        self.wv = tf.keras.layers.Dense(d_model)
-
-        self.dense = tf.keras.layers.Dense(d_model)
-
-    def split_heads(self, x, batch_size):
-        """Split the last dimension into (num_heads, depth).
-        Transpose the result such that the shape is (batch_size, num_heads, seq_len, depth)
-        """
-        x = tf.reshape(x, (batch_size, -1, self.num_heads, self.depth))
-        return tf.transpose(x, perm=[0, 2, 1, 3])
-
-    def call(self, v, k, q, mask=None):
-        batch_size = tf.shape(q)[0]
-
-        q = self.wq(q)  # (batch_size, seq_len, d_model)
-        k = self.wk(k)  # (batch_size, seq_len, d_model)
-        v = self.wv(v)  # (batch_size, seq_len, d_model)
-
-        q = self.split_heads(q, batch_size)  # (batch_size, num_heads, seq_len_q, depth)
-        k = self.split_heads(k, batch_size)  # (batch_size, num_heads, seq_len_k, depth)
-        v = self.split_heads(v, batch_size)  # (batch_size, num_heads, seq_len_v, depth)
-
-        # scaled_attention.shape == (batch_size, num_heads, seq_len_q, depth)
-        # attention_weights.shape == (batch_size, num_heads, seq_len_q, seq_len_k)
-        scaled_attention, attention_weights = scaled_dot_product_attention(
-            q, k, v, mask
-        )
-
-        scaled_attention = tf.transpose(
-            scaled_attention, perm=[0, 2, 1, 3]
-        )  # (batch_size, seq_len_q, num_heads, depth)
-
-        concat_attention = tf.reshape(
-            scaled_attention, (batch_size, -1, self.d_model)
-        )  # (batch_size, seq_len_q, d_model)
-
-        output = self.dense(concat_attention)  # (batch_size, seq_len_q, d_model)
-
-        return output, attention_weights
-
-
-def point_wise_feed_forward_network(d_model, dff):
-    return tf.keras.Sequential(
-        [
-            tf.keras.layers.Dense(dff, activation="relu"),  # (batch_size, seq_len, dff)
-            tf.keras.layers.Dense(d_model),  # (batch_size, seq_len, d_model)
-        ]
-    )
-
-
-class EncoderLayer(tf.keras.layers.Layer):
-    def __init__(self, d_model, num_heads, dff, rate=0.1):
-        super(EncoderLayer, self).__init__()
-
-        self.mha = MultiHeadAttention(d_model, num_heads)
-        self.ffn = point_wise_feed_forward_network(d_model, dff)
-
-        self.layernorm1 = tf.keras.layers.LayerNormalization(epsilon=1e-6)
-        self.layernorm2 = tf.keras.layers.LayerNormalization(epsilon=1e-6)
-
-        self.dropout1 = tf.keras.layers.Dropout(rate)
-        self.dropout2 = tf.keras.layers.Dropout(rate)
-
-    def call(self, x, training=None, mask=None):
-        attn_output, _ = self.mha(x, x, x, mask)  # (batch_size, input_seq_len, d_model)
-        attn_output = self.dropout1(attn_output, training=training)
-        out1 = self.layernorm1(x + attn_output)  # (batch_size, input_seq_len, d_model)
-
-        ffn_output = self.ffn(out1)  # (batch_size, input_seq_len, d_model)
-        ffn_output = self.dropout2(ffn_output, training=training)
-        out2 = self.layernorm2(
-            out1 + ffn_output
-        )  # (batch_size, input_seq_len, d_model)
-
-        return out2
-
-
-class Encoder(tf.keras.layers.Layer):
-    def __init__(
-        self, num_layers, d_model, num_heads, dff, maximum_position_encoding, rate=0.1,
-    ):
-        super(Encoder, self).__init__()
-
-        self.d_model = d_model
-        self.num_layers = num_layers
-
-        self.pos_encoding = positional_encoding(maximum_position_encoding, self.d_model)
-
-        self.enc_layers = [
-            EncoderLayer(d_model, num_heads, dff, rate) for _ in range(num_layers)
-        ]
-
-        self.dropout = tf.keras.layers.Dropout(rate)
-
-    def call(self, x, training=None, mask=None):
-        seq_len = tf.shape(x)[1]
-
-        x *= tf.math.sqrt(tf.cast(self.d_model, tf.float32))
-        x += self.pos_encoding[:, :seq_len, :]
-
-        x = self.dropout(x, training=training)
-
-        for i in range(self.num_layers):
-            x = self.enc_layers[i](x, training, mask)
-
-        return x  # (batch_size, input_seq_len, d_model)
-
-
-class CustomSchedule(tf.keras.optimizers.schedules.LearningRateSchedule):
-    def __init__(self, d_model, warmup_steps=4000, name=None):
-        super(CustomSchedule, self).__init__()
-
-        self.d_model = d_model
-        self.d_model = tf.cast(self.d_model, tf.float32)
-
-        self.warmup_steps = warmup_steps
-        self.name = name  # Modified from the source
-
-    def __call__(self, step):
-        arg1 = tf.math.rsqrt(step)
-        arg2 = step * (self.warmup_steps ** -1.5)
-
-        return tf.math.rsqrt(self.d_model) * tf.math.minimum(arg1, arg2)
-
-    def get_config(self):  # Modified from the source
-        return {
-            "d_model": self.d_model,
-            "warmup_steps": self.warmup_steps,
-            "name": self.name,
-        }
-
-def get_transformer_pretrain(
-    num_layers=4, d_model=128, num_heads=8, dff=256, maximum_position_encoding=2048,
-):
-    inp = Input((None, d_model))
-
-    encoder = Encoder(
-        num_layers=num_layers,
-        d_model=d_model,
-        num_heads=num_heads,
-        dff=dff,
-        maximum_position_encoding=maximum_position_encoding,
-        rate=0.3,
-    )
-
-    x = encoder(inp)
-
-    out = Dense(d_model, activation="linear", name="out_pretraining")(x)
-
-    model = Model(inputs=inp, outputs=out)
-
-    opt = Adam(0.0001) # Need Custom LR
-
-    model.compile(optimizer=opt, loss=mae)
-
-    model.summary()
-
-    return model
-
-
-
 ## Checkpoints and other stuffs.
-h5_name = os.path.join("SAVED_THINGS", "transformer.h5")
+h5_name = FILE_MODEL_CHECKPOINT_BEST
+# os.path.join("SAVED_THINGS", "transformer.h5")
 
 checkpoint_single_path = ModelCheckpoint(
         h5_name,
@@ -407,13 +182,9 @@ checkpoint_single_path = ModelCheckpoint(
         # overwrite=True
 )
 
+"""### Transformer For Audio Classification codes taken from the following GitHub [repository](https://github.com/facundodeza/transfomer-audio-classification/blob/master/audio_classification_transformer.ipynb)"""
 
-
-
-
-"""## Other Notebook's Model"""
-
-## @title Full Transformer Code from above GitHub repository
+# @title Full Transformer Code from above GitHub repository
 
 def scaled_dot_product_attention(query, key, value, mask):
   matmul_qk = tf.matmul(query, key, transpose_b=True)
@@ -672,14 +443,19 @@ class CustomLearningRateScheduler(tf.keras.optimizers.schedules.LearningRateSche
             "name": self.name,
         }
 
-NUM_LAYERS = 2
+
+
+"""## Hyperparameter Settings"""
+
+EPOCHS = 50
+
+NUM_LAYERS = 6
 D_MODEL = X.shape[2] # eg. X.shape = (7711, 1292, 13) -> D_MODEL = 13
-NUM_HEADS = 2 # 4
+NUM_HEADS = 4
 UNITS = 1024
 DROPOUT = 0.1
 TIME_STEPS = X.shape[1] # TIME_STEPS -> 1292
 OUTPUT_SIZE = NUM_LABELS
-EPOCHS = 10
 # EXPERIMENTS=10
 projection = 'none'
 
@@ -702,8 +478,6 @@ callbacks_list = [checkpoint_single_path] # , callback_lr]
 
 # CustomLearningRateScheduler(d_model=D_MODEL)
 
-
-
 lr_schedule = CustomLearningRateScheduler(d_model=D_MODEL)
 optimizer_adam_custom = tf.keras.optimizers.Adam(learning_rate=lr_schedule)
 
@@ -712,18 +486,16 @@ model.compile(optimizer=optimizer_adam_custom,
               metrics=['accuracy'])
 
 # print(model.summary())
-model.save("MODEL.h5")
-# tf.keras.models.save_model( model, "MODEL.h5", overwrite=True, include_optimizer=True, save_format=None )
-# # model.to_json("MODEL.json")
-print("Saved")
+history = model.fit(X_train, Y_train, epochs=EPOCHS, validation_data=(X_val, Y_val), callbacks=callbacks_list, verbose=1)
 
-history = model.fit(X_train[0:10], Y_train[0:10], epochs=2, validation_data=(X_test[0:10], Y_test[0:10]), callbacks = callbacks_list, verbose=1)
+model.save(FILE_MODEL_PATH)
+print(f"Model Saved to {FILE_MODEL_PATH}")
 
 
 
 """## Testing loading and reload weights"""
 
-new_model = keras.models.load_model("MODEL.h5", 
+new_model = keras.models.load_model(FILE_MODEL_PATH, 
 custom_objects={
     'PositionalEncoding': PositionalEncoding,
     'MyMultiHeadAttention': MyMultiHeadAttention
@@ -731,63 +503,31 @@ custom_objects={
 
 new_model.load_weights(h5_name)
 
-# for l1, l2, i in zip(model.layers, new_model.layers, np.arange(0, len(new_model.layers))):
-#     print(f"Check for idx {i}")
-
-#     if len(l1.weights) > 0 and len(l2.weights) > 0:
-#         print((l1.weights == l2.weights).any())
-
-
-
-
-
-
-
-
-
-# model.fit -> use as param: callbacks=[checkpoint]
-
-
-
-
-
-
-
-
-
-
-
-
-
 
 
 """## Testing"""
 
-print(np.unique(np.argmax(Y, axis=-1), return_counts=True))
+# print(np.unique(np.argmax(Y, axis=-1), return_counts=True))
 
-Y_pred_test = model.predict(X_test)
-
-
-Y_test_labeled = np.argmax(Y_test, axis=-1)
-Y_test_labeled = [get_label_name(id) for id in Y_test_labeled]
-print(Y_test_labeled)
-
-Y_pred_labeled = np.argmax(Y_pred_test, axis=-1)
-Y_pred_labeled = [get_label_name(id) for id in Y_pred_labeled]
-print(Y_pred_labeled)
-
-c_report = classification_report(y_true=Y_test_labeled, y_pred=Y_pred_labeled)
-print(c_report)
-
-plt.plot(history.history['loss'])
-plt.plot(history.history['val_loss'])
-plt.title('Model Loss')
-plt.ylabel('loss')
-plt.xlabel('epoch')
-plt.legend(['train', 'val'], loc='upper left')
-plt.show()
+# Y_pred_test = model.predict(X_test)
 
 
+# Y_test_labeled = np.argmax(Y_test, axis=-1)
+# Y_test_labeled = [get_label_name(id) for id in Y_test_labeled]
+# print(Y_test_labeled)
 
+# Y_pred_labeled = np.argmax(Y_pred_test, axis=-1)
+# Y_pred_labeled = [get_label_name(id) for id in Y_pred_labeled]
+# print(Y_pred_labeled)
 
+# c_report = classification_report(y_true=Y_test_labeled, y_pred=Y_pred_labeled)
+# print(c_report)
+
+# plt.plot(history.history['loss'])
+# plt.plot(history.history['val_loss'])
+# plt.title('Model Loss')
+# plt.ylabel('loss')
+# plt.xlabel('epoch')
+# plt.legend(['train', 'val'], loc='upper left')
+# plt.show()
 
